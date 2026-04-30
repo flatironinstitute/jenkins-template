f903160c2f1f668495fe1dc3ed782c13736a7509
Dylan Simon <dylan@dylex.net>
2026-04-30 16:40:31 -0400

    buildPod: add gpuType

--- vars/buildPod.groovy
+++ vars/buildPod.groovy
@@ -5,6 +5,7 @@ def call(Map args, Closure body) {
   def cpus       = args.cpus ?: 4;
   def memory     = args.memory ?: "16Gi";
   def gpus       = args.gpus ?: 0;
+  def gpuType    = args.gpuType ?: "";
   String image = "$REGISTRY_PREFIX/${JOB_NAME.toLowerCase()}:$BUILD_NUMBER$tag"
 
   podTemplate(inheritFrom: 'podman', showRawYaml: false) {
