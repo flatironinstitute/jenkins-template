8372ac45a33329d1b819206631a58630da885e2f
Dylan Simon <dylan@dylex.net>
2026-04-29 23:42:04 -0400

    more dockerImage

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -1,9 +1,18 @@
 def call(String dockerfile = "Dockerfile", Closure body) {
   def IMAGE = "$REGISTRY_PREFIX/${JOB_NAME.toLowerCase()}:$BUILD_NUMBER"
-  podTemplate(inheritFrom: 'podman', defaultContainer: 'main') {
+  podTemplate(inheritFrom: 'podman') {
     node(POD_LABEL) {
-      sh "podman build -t $IMAGE -f $dockerfile"
-      sh "podman push $IMAGE"
+      container('main') {
+        sh "podman build -t $IMAGE -f $dockerfile"
+        sh "podman push $IMAGE"
+      }
+    }
+  }
+  podTemplate(inheritFrom: 'default', containers: [containerTemplate(name: 'main', image: IMAGE)]) {
+    node(POD_LABEL) {
+      container('main') {
+        body.call()
+      }
     }
   }
 }
