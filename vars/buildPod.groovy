5f50beb322f11857d729d2420e0202f246e7771a
Dylan Simon <dylan@dylex.net>
2026-04-30 15:59:06 -0400

    WIP buildPod

--- vars/buildPod.groovy
+++ vars/buildPod.groovy
@@ -1,19 +1,17 @@
-def call(
-    String dockerfile = "Dockerfile",
-    String context = ".",
-    String tag = "",
-    int cpus = 4,
-    String memory = "16Gi",
-    int gpus = 0,
-    Closure body) {
-
-  String IMAGE = "$REGISTRY_PREFIX/${JOB_NAME.toLowerCase()}:$BUILD_NUMBER$tag"
+def call(Map args, Closure body) {
+  def dockerfile = args.dockerfile ?: "Dockerfile";
+  def context    = args.context ?: ".";
+  def tag        = args.tag ?: "";
+  def cpus       = args.cpus ?: 4;
+  def memory     = args.memory ?: "16Gi";
+  def gpus       = args.gpus ?: 0;
+  String image = "$REGISTRY_PREFIX/${JOB_NAME.toLowerCase()}:$BUILD_NUMBER$tag"
 
   podTemplate(inheritFrom: 'podman') {
     node(POD_LABEL) {
       container('main') {
-        sh "podman build -t $IMAGE -f $dockerfile $context"
-        sh "podman push $IMAGE"
+        sh "podman build -t $image -f $dockerfile $context"
+        sh "podman push $image"
       }
     }
   }
@@ -27,7 +25,7 @@ def call(
         nvidia: ${gpuType}
       containers:
         - name: main
-          image: $IMAGE
+          image: $image
           command: [sleep]
           args: [99999]
           securityContext:
