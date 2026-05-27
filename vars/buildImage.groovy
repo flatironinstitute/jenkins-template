6b4bad33eda285bcc13531fea7c4defdbf518575
Dylan Simon <dylan@dylex.net>
2026-05-27 11:49:50 -0400

    buildImage: add prep argument

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,12 +2,15 @@ def call(Map args) {
   def context    = args.context ?: ".";
   def dockerfile = args.dockerfile ?: "Dockerfile";
   def buildArgs  = args.buildArgs ?: "";
+  def prep       = args.prep;
   String image = imageName(args.tag ?: "")
 
   podTemplate(inheritFrom: 'podman', showRawYaml: false) {
     node(POD_LABEL) {
       checkout scm
       container('main') {
+        if (prep)
+           prep.call()
         sh "podman build -t $image -f $context/$dockerfile $buildArgs $context"
         sh "podman push $image"
       }
