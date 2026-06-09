79fd49105eca2e0ee9b4dab4b1355cb72cdadf17
Dylan Simon <dylan@dylex.net>
2026-06-09 15:34:27 -0400

    emailFailure: include CHANGE_AUTHOR_EMAIL, if set

--- vars/emailFailure.groovy
+++ vars/emailFailure.groovy
@@ -1,6 +1,8 @@
 def call() {
-  if (currentBuild.currentResult != 'SUCCESS')
+  if (currentBuild.currentResult != 'SUCCESS') {
     emailext(subject: '$DEFAULT_SUBJECT',
       body: '$DEFAULT_CONTENT',
+      to: env.CHANGE_AUTHOR_EMAIL,
       recipientProviders: [developers()])
+  }
 }
